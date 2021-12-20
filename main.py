b537891c31d371c9a5f4320e684200578341cabe
PaytonTheMartian <payton.the.martian@gmail.com>
2021-12-20 16:23:05 -0500

    Added a get mod command!

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
+# imports
 import discord
+from discord import commands
 import os
 import sys
+import requests
 
-RAT_POEM = '''
-rat
+
+# variables
+bot = discord.Bot(
+    activity = discord.CustomActivity(
+        emoji = discord.PartialEmoji(name = '📔'),
+        name = 'Informing prople about mods.'
+    ),
+    description = 'This bot is really just a joke bot, but it has some utilities too. Keyword: some.'
+)
+
+
+# constants
+RAT_POEM = '''rat
 
 Hello darkness, my old friend...
 
@@ -20,50 +34,88 @@ Still remains...
 Within the sound, of silence
 '''
 
-GREG_RANT = '''
-STOP POSTING ABOUT GREGTECH, I'M TIRED OF SEEING IT! My friends on reddit send me memes, on discord it's fucking memes - I was in a subreddit, right? and ALLLLLLLLL of the POSTS are just GregTech stuff. I- I showed my Champion underwear to my girlfriend, and the logo I flipped it and I said, "Hey babe: When the underwear greg 😂 😂 😂"
-'''
+GREG_RANT = '''STOP POSTING ABOUT GREGTECH, I'M TIRED OF SEEING IT! My friends on reddit send me memes, on discord it's fucking memes - I was in a subreddit, right? and ALLLLLLLLL of the POSTS are just GregTech stuff. I- I showed my Champion underwear to my girlfriend, and the logo I flipped it and I said, "Hey babe: When the underwear greg 😂 😂 😂"'''
 
-class Bot(discord.Client):
-    async def on_message(self, msg):
-        if msg.author.bot: return
+
+# gets a mod and returns the json data.
+def get_mod(platform: str, slug: str):
+    m = {}
+    if platform in ('modrinth', 'md'):
+        data = requests.get(f'https://api.modrinth.com/api/v1/mod/{slug}').json()
+        team_id = data['team']
+        team_members = requests.get(f'https://api.modrinth.com/api/v1/team/{team_id}/members').json()
+        members = []
+        for m in team_members:
+            mid = m['user_id']
+            members.append(requests.get(f'https://api.modrinth.com/api/v1/user/{mid}').json()['username'])
+
+        m['name'] = data['title']
+        m['authors'] = members
+        m['summary'] = data['description']
+        m['downloadCount'] = data['downloads']
+        m['websiteUrl'] = f'https://modrinth.com/mod/{slug}'
+    elif platform in ('curseforge', 'curse', 'cf'):
+        # Thanks to NikkyAI! The CF API is confusing...
+        data = requests.get(f'https://curse.nikky.moe/api/addon/{slug}').json()
+        members = []
+        for m in data['authors']:
+            members.append(m['name'])
+
+        m['name'] = data['name']
+        m['authors'] = members
+        m['summary'] = data['summary']
+        m['downloadCount'] = data['downloadCount']
+        m['websiteUrl'] = data['websiteUrl']
+    return m
+
+
+# events
+@bot.listen('on_message')
+async def on_message(msg):
+    if msg.author.bot: return
         
-        content = msg.content.split()
-        if 'neat' in content:
-            await msg.reply(embed = discord.Embed(
-                title = 'Neat',
-                description = 'Neat is a mod by Vaskii'
-            ))
-        elif 'rat' in content:
-            await msg.reply(embed = discord.Embed(
-                title = 'Rat',
-                description = RAT_POEM
-            ))
-        elif 'greg' in content:
-            await msg.reply(embed = discord.Embed(
-                title = 'Greg',
-                description = GREG_RANT
-            ))
+    content = msg.content.split()
+    if 'neat' in content:
+        await msg.reply(embed = discord.Embed(
+            title = 'Neat',
+            description = 'Neat is a mod by Vaskii'
+        ))
+    elif 'rat' in content:
+        await msg.reply(embed = discord.Embed(
+            title = 'Rat',
+            description = RAT_POEM
+        ))
+    elif 'greg' in content:
+        await msg.reply(embed = discord.Embed(
+            title = 'Greg',
+            description = GREG_RANT
+        ))
         
-        if msg.content.lower().startswith('what is spectrum'):
-            await msg.reply(embed = discord.Embed(
-                title = 'Spectrum',
-                description = 'Spectrum is a mod by DaFuqs'
-            ))
+    if msg.content.lower().startswith('what is spectrum'):
+        await msg.reply(embed = discord.Embed(
+            title = 'Spectrum',
+            description = 'Spectrum is a mod by DaFuqs'
+        ))
 
-    async def on_ready(self):
-        await bot.change_presence(
-            status = discord.Status.online,
-            activity = discord.CustomActivity(
-                'Informing people about mods.',
-                emoji = discord.PartialEmoji(
-                    name = '📔'
-                )
-            )
-        )
 
-bot = Bot()
+# commands
+@bot.slash_command(guild_ids=[908742958214438972])
+async def mod(
+    ctx,
+    platform: commands.Option(str, 'Platform', choices=['curseforge', 'modrinth']),
+    slug: commands.Option(str, 'Mod Slug')
+):
+    '''Gets a mod, platform should be a mod platform, and slug is the CF ID, or the Modrinth slug.'''
+    data = get_mod(platform, slug)
+    await ctx.respond(embed = discord.Embed(
+        title = data['name'],
+        description = (data['summary'] +\
+                '\n---\nBy: ' + ', '.join(data['authors']) +\
+                '\n---\nDownloads: ' + str(data['downloadCount']) +\
+                '\n---\nURL: ' + data['websiteUrl'])
+    ))
 
+# main things
 token = os.getenv('TOKEN')
 if len(sys.argv) >= 2:
     token = sys.argv[1]
