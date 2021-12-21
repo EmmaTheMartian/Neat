69292d7e42fe67e42467ddc14732d689f0e95731
PaytonTheMartian <payton.the.martian@gmail.com>
2021-12-21 13:30:28 -0500

    Added a search command!

--- main.py
+++ main.py
@@ -15,6 +15,11 @@ bot = discord.Bot(
     description = 'This bot is really just a joke bot, but it has some utilities too. Keyword: some.'
 )
 
+headers = {
+    'Accept': 'application/json',
+    'x-api-key': os.getenv('CF_KEY')
+}
+
 
 # constants
 RAT_POEM = '''rat
@@ -40,7 +45,10 @@ GREG_RANT = '''STOP POSTING ABOUT GREGTECH, I'M TIRED OF SEEING IT! My friends o
 # gets a mod and returns the json data.
 def get_mod(platform: str, slug: str):
     m = {}
+
+    # Modrinth
     if platform in ('modrinth', 'md'):
+        # get data
         data = requests.get(f'https://api.modrinth.com/api/v1/mod/{slug}').json()
         team_id = data['team']
         team_members = requests.get(f'https://api.modrinth.com/api/v1/team/{team_id}/members').json()
@@ -49,23 +57,29 @@ def get_mod(platform: str, slug: str):
             mid = m['user_id']
             members.append(requests.get(f'https://api.modrinth.com/api/v1/user/{mid}').json()['username'])
 
+        # set return values
         m['name'] = data['title']
         m['authors'] = members
         m['summary'] = data['description']
         m['downloadCount'] = data['downloads']
         m['websiteUrl'] = f'https://modrinth.com/mod/{slug}'
+    # CF
     elif platform in ('curseforge', 'curse', 'cf'):
-        # Thanks to NikkyAI! The CF API is confusing...
+        # thanks to NikkyAI! The CF API is confusing...
+        # get data
         data = requests.get(f'https://curse.nikky.moe/api/addon/{slug}').json()
         members = []
         for m in data['authors']:
             members.append(m['name'])
 
+        # set return values
         m['name'] = data['name']
         m['authors'] = members
         m['summary'] = data['summary']
         m['downloadCount'] = data['downloadCount']
         m['websiteUrl'] = data['websiteUrl']
+    
+    # return... it says "return m"... **RETURN** m
     return m
 
 
@@ -103,7 +117,7 @@ async def on_message(msg):
 async def mod(
     ctx,
     platform: commands.Option(str, 'Platform', choices=['curseforge', 'modrinth']),
-    slug: commands.Option(str, 'Mod Slug')
+    slug: commands.Option(str, 'Mod ID (Slug or Project ID)')
 ):
     '''Gets a mod, platform should be a mod platform, and slug is the CF ID, or the Modrinth slug.'''
     data = get_mod(platform, slug)
@@ -115,10 +129,41 @@ async def mod(
                 '\n---\nURL: ' + data['websiteUrl'])
     ))
 
+
+@bot.slash_command()
+async def search(
+    ctx,
+    platform: commands.Option(str, 'Platform', choices=['curseforge', 'modrinth']),
+    search_text: commands.Option(str, 'Search Text')
+):
+    '''Searches for a mod with the provided text.'''
+    mods = []
+    
+    # search on Modrinth
+    if platform == 'modrinth':
+        r = requests.get(f'https://api.modrinth.com/api/v1/mod?query={search_text}')
+        for mod in r.json()['hits']:
+            mods.append(f'- {mod["title"]} (**Slug**: *{mod["slug"]}*)\n')
+    # search on CF
+    elif platform == 'curseforge':
+        r = requests.get(f'https://api.curseforge.com/v1/mods/search?gameId=432&searchFilter={search_text}', headers = headers)
+        for mod in r.json()['data']:
+            mods.append(f'- {mod["name"]} (**ID**: *{mod["id"]}*, **Slug**: *{mod["slug"]}*)\n')
+    
+    # send
+    await ctx.respond(embed = discord.Embed(
+        title = 'Search',
+        description = ''.join(mods)
+    ))
+
+
 # main things
 token = os.getenv('TOKEN')
+
 if len(sys.argv) >= 2:
     token = sys.argv[1]
+if len(sys.argv) >= 3:
+    headers['x-api-key'] = sys.argv[2]
 
 bot.run(token)
 
