470621eb0defbc8aa9217eee77bc33cc17602906
EmmaTheMartian <payton.the.martian@gmail.com>
2024-01-24 22:09:07 -0500

    Basically added most of the stuff

--- src/main.py
+++ src/main.py
@@ -1,16 +1,28 @@
+# ANCHOR: Imports
 import discord
 import modutil
+import os
+import random
+from typing import Union
 
+
+# ANCHOR: Variables
 bot = discord.Bot(
 	activity = discord.CustomActivity(
 		emoji = discord.PartialEmoji(name = 'sob'),
 		name = 'Informing people about mods.'
 	),
-	description = 'Informs you about Neat, GregTech, Rats, and anything else you can find.'
+	description = 'Informs you about Neat, GregTech, Rats, and anything else you can find on CurseForge or Modrinth.',
+	intents = discord.Intents(
+		message_content = True,
+		messages = True,
+		presences = True,
+	)
 )
 
 
 NO_PROFANITY = True
+DISABLE_MEMES = False
 RESPONSE_NEAT = 'Neat is a mod by Vazkii'
 RESPONSE_GREG = '''STOP POSTING ABOUT GREGTECH, I'M TIRED OF SEEING IT! My friends on reddit send me memes, on discord it's ''' + ('' if NO_PROFANITY else 'fucking' ) + '''memes - I was in a subreddit, right? and ALLLLLLLLL of the POSTS are just GregTech stuff. I- I showed my Champion underwear to my girlfriend, and the logo I flipped it and I said, "Hey babe: When the underwear greg 😂 😂 😂"'''
 RESPONSE_RAT = '''All I know how to do is reply "haha funny rat mod" every time someone says "rat" in the reddit comments. Is there more to life? Is there more to existence? I wouldn't know. It's not in my code.'''
@@ -25,38 +37,63 @@ Still remains...
 Within the sound, of silence
 '''
 ERRORS = {
-	'unknown_mod_platform': lambda arg: f'Unknown mod platform: {arg}'
+	'unknown_mod_platform': lambda arg: f'Unknown mod platform: {arg}',
+	'no_such_mod': lambda arg: f'No such mod: {arg}'
 }
 
 
-def embed(title: str, description: str):
-	return discord.Embed(title = title, description = description)
+# ANCHOR: Utility functions
+def embed(title: str, description: str, **kwargs):
+	return discord.Embed(
+		title = title,
+		description = description,
+		color = discord.Color.random(),
+		**kwargs
+	)
 
 def error(code: str, *args):
-	return embed('Error', ERRORS[code](*args))
+	e = embed('Error', ERRORS[code](*args))
+	e.color = discord.Color.dark_red()
+	return e
 
 _PLATFORMS = ['curseforge', 'modrinth', 'cf', 'mr']
 def _get_platforms(c: discord.AutocompleteContext):
 	return [p for p in _PLATFORMS if p.startswith(c.value.lower())]
 
+def _get_category_filters(c: discord.AutocompleteContext):
+	return [
+		f for f in (
+			modutil._CF_CATEGORIES if c.options['platform'] in {'cf', 'curseforge'} else
+			modutil._MR_CATEGORIES if c.options['platform'] in {'mr', 'modrinth'} else
+			[]
+		) if f.startswith(c.value.lower())
+	]
+
+# ANCHOR: Bot events
+@bot.listen('on_ready')
+async def on_ready():
+	print('Bot ready!')
+
+
 @bot.listen('on_message')
 async def on_message(msg):
-	if msg.author.bot:
+	if DISABLE_MEMES or msg.author.bot:
 		return
 
 	content = msg.content.lower()
-	words = content.split()()
+	words = [word.strip('!@#$%^&*()_+-=[]{}\\|~`;:\'",.<>/?') for word in content.split()]
 
 	if content.startswith('what is spectrum'):
-		await msg.reply(embed('Spectrum', RESPONSE_SPECTRUM))
+		await msg.reply(embed = embed('Spectrum', RESPONSE_SPECTRUM))
 	elif 'neat' in words:
-		await msg.reply(embed('Neat', RESPONSE_NEAT))
+		await msg.reply(embed = embed('Neat', RESPONSE_NEAT, thumbnail = 'https://media.forgecdn.net/avatars/588/684/637958998850280257.png'))
 	elif 'rat' in words:
-		await msg.reply(embed('Rat', RESPONSE_RAT))
+		await msg.reply(embed = embed('Rat', RESPONSE_RAT))
 	elif 'greg' in words or 'gregtech' in words:
-		await msg.reply(embed('Greg', RESPONSE_GREG))
+		await msg.reply(embed = embed('Greg', RESPONSE_GREG))
 
 
+# ANCHOR: Slash commands
 @bot.slash_command(name = 'mod')
 @discord.commands.option('platform', description = 'Platform', autocomplete = _get_platforms)
 @discord.commands.option('slug', description = 'Project ID or Slug')
@@ -69,48 +106,51 @@ async def mod(
 	data = None
 
 	if platform in {'cf', 'curseforge'}:
-		data = modutil.Mod.from_curseforge(slug)
+		data = modutil.Mod.from_curseforge(int(slug))
 	elif platform in {'mr', 'modrinth'}:
 		data = modutil.Mod.from_modrinth(slug)
 	else:
 		await c.respond(embed = error('unknown_mod_platform', platform))
 		return
 
-	description =\
-		f'''{data.summary}
-		---
-		By: {', '.join(data.authors)}
-		---
-		Downloads: {data.downloads}
-		---
-		URL: {data.url}'''.strip('\t ')
+	if data == 404:
+		await c.respond(embed = error('no_such_mod', slug))
+		return
 
-	await c.respond(embed = embed(data.name, description))
+	e = embed(data.name, data.summary)
+	e.set_thumbnail(url = data.icon_url)
+	e.add_field(name = 'By', value = ', '.join(data.authors))
+	e.add_field(name = 'Downloads', value = data.downloads)
+	e.add_field(name = 'URL', value = data.url)
+	await c.respond(embed = e)
 
 
 @bot.slash_command(name = 'search')
 @discord.commands.option('platform', description = 'Platform', autocomplete = _get_platforms)
 @discord.commands.option('query', description = 'Search query')
+@discord.commands.option('category_filter', description = 'Apply a category filter (e.g, modpacks, mods, etc.)', default = 'mod', autocomplete = _get_category_filters)
+@discord.commands.option('show_ids', description = 'If project IDs/Slugs should be shown', input_type = bool, default = False, required = False)
 async def search(
-    ctx,
+    c,
 	platform: str,
-	query: str
+	query: str,
+	category_filter: str,
+	show_ids: bool
 ):
 	mods = []
 	if platform in {'mr', 'modrinth'}:
-		mods = modutil.search_modrinth(query)
+		mods = modutil.search_modrinth(query, category_filter)
+	elif platform in {'cf', 'curseforge'}:
+		mods = modutil.search_curseforge(query, category_filter)
+	else:
+		await c.respond(embed = error('unknown_mod_platform', platform))
+		return
 
-    # search on Modrinth
-    if platform == 'modrinth':
-		mods = modutil.search_modrinth(query)
-    # search on CF
-    elif platform == 'curseforge':
-        r = requests.get(f'https://api.curseforge.com/v1/mods/search?gameId=432&categoryId=&searchFilter={search_text}', headers = headers)
-        for mod in r.json()['data']:
-            mods.append(f'- {mod["name"]} (**ID**: *{mod["id"]}*, **Slug**: *{mod["slug"]}*)\n')
+	await c.respond(embed = embed('Search Results', '\n'.join([
+		f'- **{mod[0]}** ({mod[2]})' + ('' if not show_ids else f' [ID: {mod[1]}]') for mod in mods
+	])))
 
-    # send
-    await ctx.respond(embed = discord.Embed(
-        title = 'Search',
-        description = ''.join(mods)
-    ))
+
+# ANCHOR: if/main
+if __name__ == '__main__':
+	bot.run(os.environ['TOKEN'])
--- src/modutil.py
+++ src/modutil.py
@@ -6,13 +6,37 @@ import os
 __all__ = ('Mod',)
 
 
-URL_MR = 'https://api.modrinth.com/api/v1'
-URL_CF = 'https://curse.nikky.moe/api'
-CF_SEARCH_HEADERS = { 'Accept': 'application/json', 'x-api-key': os.getenv('CF_KEY') }
+URL_MR = 'https://api.modrinth.com/v2'
+URL_CF = 'https://api.curseforge.com/v1'
+CF_HEADERS = { 'Accept': 'application/json', 'x-api-key': os.getenv('CF_KEY') }
+_MR_CATEGORIES = {
+	'mod': 'mod',
+	'modpack': 'modpack',
+	'resourcepack': 'resourcepack',
+	'shader': 'shader'
+}
+_CF_CATEGORIES = {
+	'mod': 6,
+	'modpack': 4471,
+	'shader': 6552,
+	'resourcepack': 12,
+	'plugin': 5,
+	'world': 17,
+	'addon': 4559,
+}
 
 
 def _get(url: str, *args, **kwargs):
-	return requests.get(url, *args, **kwargs).json()
+	try:
+		return requests.get(url, *args, **kwargs)
+	except Exception as e:
+		print(f'\033[31;1m_get error ({r}):\033[0m {e}')
+
+def _get_mr(url: str, *args, **kwargs):
+	return _get(URL_MR + url, *args, *kwargs)
+
+def _get_cf(url: str, *args, **kwargs):
+	return _get(URL_CF + url, headers = CF_HEADERS, *args, **kwargs)
 
 
 class Mod(NamedTuple):
@@ -21,14 +45,19 @@ class Mod(NamedTuple):
 	authors: list[str]
 	downloads: int
 	url: str
+	icon_url: str
 
 	@staticmethod
 	def from_modrinth(slug: str):
-		data = _get(f'{URL_MR}/mod/{slug}')
+		data = _get_mr(f'/project/{slug}')
+		if data.status_code == 404:
+			return 404
+
+		data = data.json()
 		members = [
-			_get(f'{URL_MR}/user/{member_id}')['username']
-			for member_id in
-			_get(f'{URL_MR}/team/{data['team']}/members')
+			member['user']['username']
+			for member in
+			_get_mr(f'/team/{data['team']}/members').json()
 		]
 
 		return Mod(
@@ -36,33 +65,47 @@ class Mod(NamedTuple):
 			data['description'],
 			members,
 			data['downloads'],
-			f'https://modrinth.com/mod/{slug}'
+			f'https://modrinth.com/mod/{slug}',
+			data['icon_url']
 		)
 
 	@staticmethod
-	def from_curseforge(slug: str):
-		data = _get(f'{URL_CF}/addon/{slug}')
+	def from_curseforge(project_id: int):
+		data = _get_cf(f'/mods/{project_id}')
+		if data.status_code == 404:
+			return 404
+
+		data = data.json()['data']
 		return Mod(
 			data['name'],
 			data['summary'],
 			[user['name'] for user in data['authors']],
 			data['downloadCount'],
-			data['websiteUrl']
+			data['links']['websiteUrl'],
+			data['logo']['url']
 		)
 
 
-def search_modrinth(query: str):
+def search_modrinth(query: str, category: str = None):
 	"""Performs a search using a specific query on Modrinth. Only returns the title, slug, and URL of each mod!"""
+	category = 'mod' if category is None else category
 	return [
-		(hit['title'], hit['slug'], f'https://modrinth.com/mod/{hit['slug']}')
+		(hit['title'], hit['slug'], f'https://modrinth.com/{_MR_CATEGORIES[category]}/{hit['slug']}')
 		for hit in
-		_get(f'{URL_MR}/mod?query={query}')['hits']
+		_get_mr(f'/search?query={query}&facets=[["project_type:{category}"]]').json()['hits']
 	]
 
-def search_curseforge(query: str):
+
+def search_curseforge(query: str, category: str = None):
 	"""Performs a search using a specific query on Curseforge. Only returns the title, slug, and URL of each mod!"""
+	category = category if category is not None else 'mod'
 	return [
-		(hit['name'], hit['id'], hit['websiteUrl'])
+		(hit['name'], hit['id'], hit['links']['websiteUrl'])
 		for hit in
-		_get(f'https://api.curseforge.com/v1/mods/search?gameId=432&categoryId=&searchFilter={query}', headers = CF_SEARCH_HEADERS)['data']
+		_get_cf(f'/mods/search', params = {
+			'gameId': 432,
+			'searchFilter': query,
+			'classId': _CF_CATEGORIES[category],
+			'pageSize': 10
+		}).json()['data']
 	]
